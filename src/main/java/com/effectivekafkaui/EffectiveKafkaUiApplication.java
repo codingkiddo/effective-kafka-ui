bb35f44bfe9e0aedb4aa8f3924db09781ad395c1
Vinod Kumar <vinodkumar@192.168.1.8>
2025-08-21 20:39:00 +0530

    Initial Commit

---
+++ src/main/java/com/effectivekafkaui/EffectiveKafkaUiApplication.java
@@ -0,0 +1,13 @@
+package com.effectivekafkaui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveKafkaUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveKafkaUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/effectivekafkaui/EffectiveKafkaUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effectivekafkaui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveKafkaUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
